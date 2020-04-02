1632e86fcdb541c9c6817a1090a4d1e529080163
fqjin <fqjin@users.noreply.github.com>
2020-04-02 12:30:32 -0400

    Add auto_random play

--- src/manual.py
+++ src/manual.py
@@ -1,32 +1,55 @@
 """Play UTTT Manually"""
+from random import randrange
 from engine import *
 
 
-if __name__ == '__main__':
+def play_manual(auto_random=False, verbose=True):
     game = BigBoard()
-    game.draw()
+    if verbose:
+        game.draw()
 
     while True:
-        print(f'Mover: {decode_dict[game.mover+1]}')
-        if len(game.sectors) > 1:
-            sector = int(input('Sector: '))
-        else:
-            sector = game.sectors[0]
-            print(f'Sector {sector}')
-        tile = int(input('Tile: '))
+        if not auto_random:
+            print(f'Mover: {decode_dict[game.mover+1]}')
+        while True:
+            if len(game.sectors) > 1:
+                if auto_random:
+                    sector = randrange(9)
+                else:
+                    sector = int(input('Sector: '))
+            else:
+                sector = game.sectors[0]
+                if not auto_random:
+                    print(f'Sector {sector}')
+            if auto_random:
+                tile = randrange(9)
+            else:
+                tile = int(input('Tile: '))
 
-        try:
-            game.move(sector, tile)
-            game.draw()
+            try:
+                game.move(sector, tile)
+                break
+            except ValueError:
+                if not auto_random:
+                    print('Illegal Move')
+
+        if verbose:
+            print(f'{decode_dict[2-game.mover]} @ {sector}, {tile}')
             print(game.states)
             print(game.secret_states)
-        except ValueError:
-            print('Illegal Move')
+            game.draw()
 
         if game.result:
             print('GAME OVER')
             if game.result == 3:
                 print('Tie Game')
-            elif game.result == 1:
+            else:
                 print(f'{decode_dict[game.result]} Wins')
-            break
+            return game.result
+
+
+if __name__ == '__main__':
+    # play_manual(auto_random=True)
+    results = [play_manual(auto_random=True, verbose=False) for _ in range(100)]
+    print(results)
+    print(np.histogram(results, bins=3))
