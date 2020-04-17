4a54839f2cdd9990b6928840636b2fa5718604e1
fqjin <fqjin@users.noreply.github.com>
2020-04-17 13:31:33 -0400

    Add net3000 results

--- selfplay/scramble.py
+++ selfplay/scramble.py
@@ -9,8 +9,8 @@ if os.path.exists('./scramble'):
 os.mkdir('./scramble')
 
 n = len(glob.glob('./*.npz'))
+print(f'Got {n} games')
 permute = np.arange(n).reshape((-1, 500)).flatten('F')
-# 'F' mode flattens by column, so games are evenly distributed
 for i, j in enumerate(permute):
     shutil.copy(f'./{str(j).zfill(5)}.npz''',
                 f'./scramble/{str(i).zfill(5)}.npz')
