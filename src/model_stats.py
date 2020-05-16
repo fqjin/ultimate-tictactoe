b9f01dad69e6aa920b4f316e7cd325d4cea921df
fqjin <fqjin@users.noreply.github.com>
2020-05-15 20:45:28 -0400

    Add net6000 results

--- src/model_stats.py
+++ src/model_stats.py
@@ -1,7 +1,7 @@
 import torch
 from stats import make_stats
 from network import UTTTNet
-from net_player import NetPlayer
+from net_player import BatchNetPlayer
 
 
 def model_vs_model(weights1, weights2, device='cpu', nodes=100, ):
@@ -11,8 +11,8 @@ def model_vs_model(weights1, weights2, device='cpu', nodes=100, ):
     m2.load_state_dict(torch.load(f'../models/{weights2}.pt', map_location=device))
     m1.to(device).eval()
     m2.to(device).eval()
-    p1 = NetPlayer(nodes=nodes, v_mode=True, model=m1, device=device, noise=False)
-    p2 = NetPlayer(nodes=nodes, v_mode=True, model=m2, device=device, noise=False)
+    p1 = BatchNetPlayer(nodes=nodes, v_mode=True, model=m1, device=device, noise=False)
+    p2 = BatchNetPlayer(nodes=nodes, v_mode=True, model=m2, device=device, noise=False)
     make_stats(p1, p2, weights1, weights2, weights1+'_vs_'+weights2, num=50, temp=(3, 0.5))
 
 
@@ -20,12 +20,14 @@ def model_VN(weights1, device='cpu', nodes=100, ):
     m = UTTTNet()
     m.load_state_dict(torch.load(f'../models/{weights1}.pt', map_location=device))
     m.to(device).eval()
-    p1 = NetPlayer(nodes=nodes, v_mode=True, model=m, device=device, noise=False)
-    p2 = NetPlayer(nodes=nodes, v_mode=False, model=m, device=device, noise=False)
+    p1 = BatchNetPlayer(nodes=nodes, v_mode=True, model=m, device=device, noise=False)
+    p2 = BatchNetPlayer(nodes=nodes, v_mode=False, model=m, device=device, noise=False)
     make_stats(p1, p2, 'Vmode', 'Nmode', weights1+'_V_vs_N', num=50, temp=(3, 0.5))
 
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Using {device}')
     base = '00000'
     net1 = 'newplane_from00000_100_1000bs2048lr0.1d0.001e3'
     net2 = 'from00000_150_1500bs2048lr0.1d0.001e3'
@@ -36,14 +38,16 @@ if __name__ == '__main__':
     net7 = '400_4000bs2048lr0.1d0.001e4'
     net8 = '450_4500bs2048lr0.1d0.001e4'
     net9 = '500_5000bs2048lr0.1d0.001e4'
-    new = net9
-    # model_vs_model(base, new)
-    # model_vs_model(net1, new)
-    # model_vs_model(net2, new)
-    # model_vs_model(net3, new)
-    # model_vs_model(net4, new)
-    model_vs_model(net5, new)
-    model_vs_model(net6, new)
-    model_vs_model(net7, new)
-    model_vs_model(net8, new)
-    model_VN(new)
+    net10 = '600_6000bs2048lr0.1d0.001e4_e3'
+    new = net10
+    # model_vs_model(base, new, device=device)
+    # model_vs_model(net1, new, device=device)
+    # model_vs_model(net2, new, device=device)
+    # model_vs_model(net3, new, device=device)
+    # model_vs_model(net4, new, device=device)
+    # model_vs_model(net5, new, device=device)
+    model_vs_model(net6, new, device=device)
+    model_vs_model(net7, new, device=device)
+    model_vs_model(net8, new, device=device)
+    model_vs_model(net9, new, device=device)
+    model_VN(new, device=device)
