63e20d34b417b9f12a0d221b1f53880d352f7d46
fqjin <fqjin@users.noreply.github.com>
2020-05-23 10:17:58 -0400

    Add net19000 results

--- src/selfplay.py
+++ src/selfplay.py
@@ -16,7 +16,7 @@ def selfplay(nodes, number, model, device='cpu'):
     np.savez(savepath, result=result, moves=moves, visits=savelist, evals=evals)
 
 
-best_net = '1800_18000bs2048lr0.1d0.001e8'
+best_net = '1900_19000bs2048lr0.1d0.001e5'
 
 
 if __name__ == '__main__':
