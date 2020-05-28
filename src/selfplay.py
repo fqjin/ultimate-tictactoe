07bcd44517a20d42703fb0b91cc34991c32206e3
fqjin <fqjin@users.noreply.github.com>
2020-05-27 23:19:19 -0400

    Add net24000 results

--- src/selfplay.py
+++ src/selfplay.py
@@ -16,7 +16,7 @@ def selfplay(nodes, number, model, device='cpu'):
     np.savez(savepath, result=result, moves=moves, visits=savelist, evals=evals)
 
 
-best_net = '2200_22000bs2048lr0.1d0.001e5'
+best_net = '2400_24000bs2048lr0.1d0.001e6'
 
 
 if __name__ == '__main__':
