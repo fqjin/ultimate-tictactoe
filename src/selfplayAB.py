368888faf51c5af1fb9332cdc0f2cb0fdc39bbd8
fqjin <fqjin@users.noreply.github.com>
2020-11-01 10:42:24 -0500

    Update selfplayAB to use neural network

--- src/selfplayAB.py
+++ src/selfplayAB.py
@@ -1,10 +1,16 @@
 import numpy as np
+import torch
 from alphabeta import ABPlayer, RandABTree
+from ab_model import NetABTree, load_ABnet
 from play import play
 
 
+best_net = '1000_10000bs2048lr0.1d0.001abe5'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
 def selfplayAB(number, player):
-    result, moves, evals = play(player, player, verbose=False)
+    result, moves, evals = play(player, player, temp=(7, 1.0))
 
     savepath = '../selfplayAB/' + str(number).zfill(5)
 
@@ -19,7 +25,11 @@ if __name__ == '__main__':
     parser.add_argument('--range', type=int, nargs=2, required=True)
     args = parser.parse_args()
 
-    p = ABPlayer(RandABTree, max_depth=3, selfplay=True)
+    # p = ABPlayer(RandABTree, max_depth=3, selfplay=True)
+
+    model = load_ABnet(best_net, device=device)
+    p = ABPlayer(NetABTree, max_depth=3, selfplay=True,
+                 model=model, device=device)
     for i in range(*args.range):
         print(i)
         # With batching, GPU is 4x faster than CPU, ~26 sec per game
